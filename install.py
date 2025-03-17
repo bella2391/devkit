3e5071920fcfcdae5797d14ed3bd36fc64a43bdc
bella <maekawatakaya10.8@gmail.com>
2025-03-17 22:23:35 +0900

    add password for user

--- install.py
+++ install.py
@@ -5,21 +5,20 @@ env_file = ".env"
 
 default_user = "user"
 default_group = "users"
-default_user_id = "1000"
 
 if not os.path.exists(env_file) or \
    "DOCKER_USER" not in open(env_file).read() or \
-   "DOCKER_GROUP" not in open(env_file).read() or \
-   "DOCKER_USER_ID" not in open(env_file).read():
+   "DOCKER_USER_PASSWD" not in open(env_file).read() or \
+   "DOCKER_GROUP" not in open(env_file).read():
 
     docker_user = input(f"Enter Docker username (default: {default_user}): ") or default_user
+    docker_user_passwd = input(f"Enter Docker user password (default: {default_user}): ") or default_user
     docker_group = input(f"Enter Docker group (default: {default_group}): ") or default_group
-    docker_user_id = input(f"Enter Docker user ID (default: {default_user_id}): ") or default_user_id
 
     with open(env_file, "w") as f:
         f.write(f"DOCKER_USER={docker_user}\n")
+        f.write(f"DOCKER_USER_PASSWD={docker_user_passwd}\n")
         f.write(f"DOCKER_GROUP={docker_group}\n")
-        f.write(f"DOCKER_USER_ID={docker_user_id}\n")
 
     print(f"Environment variables saved to {env_file}")
 
