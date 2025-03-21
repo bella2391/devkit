c9aad167bbb4baf3660b77ed374f6f1c73ff1ced
bella <maekawatakaya10.8@gmail.com>
2025-03-21 23:38:20 +0900

    fix output `\n`, using echo command -e flag

--- scripts/first-setup.sh
+++ scripts/first-setup.sh
@@ -12,7 +12,7 @@ if [ -f "/.dockerenv" ]; then
     rm /.dockerenv
 fi
 
-echo -e "\nGenerating pacman lsign key..." && pacman-key --init 2> /dev/null && echo "Done\n"
+echo -e "\nGenerating pacman lsign key..." && pacman-key --init 2> /dev/null && echo -e "Done\n"
 
 # See https://gitlab.archlinux.org/archlinux/archlinux-wsl/-/issues/3
 systemctl cancel "$(systemctl list-jobs | grep systemd-firstboot.service | awk '{print $1}')" 2> /dev/null || true
