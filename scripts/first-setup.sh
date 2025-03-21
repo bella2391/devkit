d3e0697b6ee910395ff773887da6744551897707
bella <maekawatakaya10.8@gmail.com>
2025-03-21 21:50:29 +0900

    add user git config prompt

--- scripts/first-setup.sh
+++ scripts/first-setup.sh
@@ -14,20 +14,25 @@ echo -e "\nGenerating pacman lsign key..." && pacman-key --init 2> /dev/null && 
 # See https://gitlab.archlinux.org/archlinux/archlinux-wsl/-/issues/3
 systemctl cancel "$(systemctl list-jobs | grep systemd-firstboot.service | awk '{print $1}')" 2> /dev/null || true
 
-# Git config
-while true; do
-  read -p "Enter your github email address: " email
-  if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-    break
-  else
-    echo "Invalid email address. Please try again."
-  fi
-done
+# Git setup prompt
+read -p "Do you setup github config? (y/n): " setup_github
+if [[ "$setup_github" == "y" ]]; then
+  while true; do
+    read -p "Enter your github email address: " email
+    if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+      break
+    else
+      echo "Invalid email address. Please try again."
+    fi
+  done
 
-read -p "Enter your github name: " name
+  read -p "Enter your github name: " name
 
-git config --global user.email "$email"
-git config --global user.name "$name"
+  sudo -u "$user" git config --global user.email "$email"
+  sudo -u "$user" git config --global user.name "$name"
 
-echo "Git configuration completed."
+  echo "Git configuration completed."
+else
+    echo "Github setup skipped."
+fi
 
