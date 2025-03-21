12b09b366342aba9b53068548143e71f800fef3d
bella <maekawatakaya10.8@gmail.com>
2025-03-21 23:39:11 +0900

    use condition whether exists file and dir or not, and adapt log style

--- scripts/first-setup.sh
+++ scripts/first-setup.sh
@@ -34,6 +34,15 @@ if [[ "$setup_github" == "y" ]]; then
   # sudo -u "$user" git config --global user.email "$email"
   # sudo -u "$user" git config --global user.name "$name"
 
+  if [ ! -L /run/user/1000/wayland-0 ]; then
+    if [ -d /mnt/wslg/runtime-dir ]; then
+      sudo -u "$user" ln -s /mnt/wslg/runtime-dir/wayland-0* /run/user/1000/
+      echo "\[$(date '+%Y-%m-%d %H:%M:%S')\] CREATED WAYLAND SYMLINK FROM FIRST-SETUP" >> $HOME/.wsl.log
+    else
+      echo "Error: /mnt/wslg/runtime-dir not found."
+    fi
+  fi
+
   sudo -u "$user" git-credential-manager github login --no-ui
   echo "Git configuration completed."
 else
