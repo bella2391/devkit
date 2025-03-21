172fe7742fdee6a64cdf7d8bce606003f10e9107
bella <maekawatakaya10.8@gmail.com>
2025-03-21 23:07:57 +0900

    test for git-credential-manager login with no ui

--- scripts/first-setup.sh
+++ scripts/first-setup.sh
@@ -18,22 +18,23 @@ echo -e "\nGenerating pacman lsign key..." && pacman-key --init 2> /dev/null && 
 systemctl cancel "$(systemctl list-jobs | grep systemd-firstboot.service | awk '{print $1}')" 2> /dev/null || true
 
 # Git setup prompt
-read -p "Do you setup github config? (y/n): " setup_github
+read -p "Do you setup github config? (require login in your browser) (y/n): " setup_github
 if [[ "$setup_github" == "y" ]]; then
-  while true; do
-    read -p "Enter your github email address: " email
-    if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-      break
-    else
-      echo "Invalid email address. Please try again."
-    fi
-  done
+  # while true; do
+  #   read -p "Enter your github email address: " email
+  #   if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+  #     break
+  #   else
+  #     echo "Invalid email address. Please try again."
+  #   fi
+  # done
 
-  read -p "Enter your github name: " name
+  # read -p "Enter your github name: " name
 
-  sudo -u "$user" git config --global user.email "$email"
-  sudo -u "$user" git config --global user.name "$name"
+  # sudo -u "$user" git config --global user.email "$email"
+  # sudo -u "$user" git config --global user.name "$name"
 
+  sudo -u "$user" git-credential-manager github login --no-ui
   echo "Git configuration completed."
 else
     echo "Github setup skipped."
