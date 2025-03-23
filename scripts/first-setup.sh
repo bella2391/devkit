916c9d2dc5831bb06282b18659f5055c8e9efe5a
bella <maekawatakaya10.8@gmail.com>
2025-03-23 12:25:09 +0900

    add condition for all commands

--- scripts/first-setup.sh
+++ scripts/first-setup.sh
@@ -12,8 +12,22 @@ if [ -f "/.dockerenv" ]; then
     rm /.dockerenv
 fi
 
-echo -e "\nGenerating pacman lsign key..." && pacman-key --init 2> /dev/null && echo -e "Done\n"
+if pacman-key --init 2> /dev/null; then
+  echo "pacman key initialized successfully."
+else
+  echo "Failed to initialize pacman key."
+fi
 
 # See https://gitlab.archlinux.org/archlinux/archlinux-wsl/-/issues/3
-systemctl cancel "$(systemctl list-jobs | grep systemd-firstboot.service | awk '{print $1}')" 2> /dev/null || true
+if systemctl cancel "$(systemctl list-jobs | grep systemd-firstboot.service | awk '{print $1}')" 2> /dev/null || true; then
+  echo "systemd-firstboot.service canceled successfully."
+else
+  echo "Failed to cancel systemd-firstboot.service."
+fi
+
+if systemctl enable --now discord.service; then
+  echo "discord.service enabled and started successfully."
+else
+  echo "Failed to enable or start discord.service."
+fi
 
